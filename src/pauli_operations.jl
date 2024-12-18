5b0c044060ad5f9db9cfa6110bfd3d04d19ac5b9
ooalshei <ooalshei@ncsu.edu>
2024-12-17 22:01:07 -0500

    Styling and input abstraction

--- src/pauli_operations.jl
+++ src/pauli_operations.jl
@@ -6,105 +6,64 @@ interchangeably referred to as (1, 2, 3, 4) or (-, X, Y, Z).
 """
 
 # These arrays are used to find products of Pauli matrices.
-const rules = Int8[1 2 3 4;
+const RULES = Int8[1 2 3 4;
     2 1 4 3;
     3 4 1 2;
     4 3 2 1]
-const sign_rules = Complex{Int8}[1 1 1 1;
+const SIGN_RULES = Complex{Int8}[1 1 1 1;
     1 1 1im -1im;
     1 -1im 1 1im;
     1 1im -1im 1]
 
-@inline function pauliprod(string1::Vector{Int8},
-    string2::Vector{Int8})::Tuple{Vector{Int8},Complex{Int8},Bool}
+function pauliprod(string1::AbstractVector{Int8},
+    string2::AbstractVector{Int8})::Tuple{Vector{Int8},Complex{Int8},Bool}
 
     length(string1) == length(string2) || throw(DimensionMismatch("Strings have different lengths ($(length(string1)) and $(length(string2)))"))
     index = CartesianIndex.(string1, string2)
-    result = rules[index]
-    sign = prod(sign_rules[index])
-    imag(sign) == 0 ? (return result, sign, true) : (return result, sign, false)
+    result = @view RULES[index]
+    sign = prod(@view SIGN_RULES[index])
+    isreal(sign) ? (return result, sign, true) : (return result, sign, false)
 end
 
-@inline function pauliprod(string1::SubArray,
-    string2::Vector{Int8})::Tuple{Vector{Int8},Complex{Int8},Bool}
+function paulisum(sentence1::AbstractDict{<:AbstractVector{Int8},T},
+    sentence2::AbstractDict{<:AbstractVector{Int8},U})::Dict{Vector{Int8},promote_type(T,U)} where {T<:Number,U<:Number}
 
-    length(string1) == length(string2) || throw(DimensionMismatch("Strings have different lengths ($(length(string1)) and $(length(string2)))"))
-    index = CartesianIndex.(string1, string2)
-    result = rules[index]
-    sign = prod(sign_rules[index])
-    imag(sign) == 0 ? (return result, sign, true) : (return result, sign, false)
-end
-
-@inline function pauliprod(string1::Vector{Int8},
-    string2::SubArray)::Tuple{Vector{Int8},Complex{Int8},Bool}
-
-    length(string1) == length(string2) || throw(DimensionMismatch("Strings have different lengths ($(length(string1)) and $(length(string2)))"))
-    index = CartesianIndex.(string1, string2)
-    result = rules[index]
-    sign = prod(sign_rules[index])
-    imag(sign) == 0 ? (return result, sign, true) : (return result, sign, false)
-end
-
-@inline function pauliprod(string1::SubArray,
-    string2::SubArray)::Tuple{Vector{Int8},Complex{Int8},Bool}
-
-    length(string1) == length(string2) || throw(DimensionMismatch("Strings have different lengths ($(length(string1)) and $(length(string2)))"))
-    index = CartesianIndex.(string1, string2)
-    result = rules[index]
-    sign = prod(sign_rules[index])
-    imag(sign) == 0 ? (return result, sign, true) : (return result, sign, false)
-end
-
-function _paulisum(sentence1::Dict{Vector{Int8},Float64},
-    sentence2::Dict{Vector{Int8},Float64})::Dict{Vector{Int8},Float64}
-
-    result = copy(sentence1)
+    result = Dict{Vector{Int8},promote_type(T,U)}(sentence1)
     for (key, value) in sentence2
         result[key] = get(result, key, 0.0) + value
     end
     return result
 end
 
-function paulisum(sentences::Dict{Vector{Int8},Float64}...;
-    tol=0.0)::Dict{Vector{Int8},Float64}
+function paulisum(sentences::AbstractDict{<:AbstractVector{Int8},<:Number}...;
+    atol::Real=0)::Dict{Vector{Int8},<:Number}
 
     result = sentences[1]
     for sentence in sentences[2:end]
-        result = _paulisum(result, sentence)
+        result = paulisum(result, sentence)
     end
     for (key, value) in result
-        abs(value) <= tol && pop!(result, key)
+        abs(value) <= atol && pop!(result, key)
     end
     length(sentences) > 1 ? (return result) : return copy(sentences[1])
 end
 
-function _paulidiff(sentence1::Dict{Vector{Int8},Float64},
-    sentence2::Dict{Vector{Int8},Float64})::Dict{Vector{Int8},Float64}
+function _paulidiff(sentence1::AbstractDict{<:AbstractVector{Int8},T},
+    sentence2::AbstractDict{<:AbstractVector{Int8},U};
+    atol::Real=0)::Dict{Vector{Int8},promote_type(T,U)} where {T<:Number,U<:Number}
 
-    result = copy(sentence1)
+    result = Dict{Vector{Int8},promote_type(T,U)}(sentence1)
     for (key, value) in sentence2
         result[key] = get(result, key, 0.0) - value
+        abs(value) <= atol && pop!(result, key)
     end
     return result
 end
 
-function paulidiff(sentences::Dict{Vector{Int8},Float64}...;
-    tol=0.0)::Dict{Vector{Int8},Float64}
+function _pauliprod(sentence1::AbstractDict{<:AbstractVector{Int8},<:Number},
+    sentence2::AbstractDict{<:AbstractVector{Int8},<:Number})::Dict{Vector{Int8},<:Number}
 
-    result = sentences[1]
-    for sentence in sentences[2:end]
-        result = _paulidiff(result, sentence)
-    end
-    for (key, value) in result
-        abs(value) <= tol && pop!(result, key)
-    end
-    length(sentences) > 1 ? (return result) : return copy(sentences[1])
-end
-
-function _pauliprod(sentence1::Dict{Vector{Int8},Number},
-    sentence2::Dict{Vector{Int8},Number})::Dict{Vector{Int8},Number}
-
-    result = Dict{Vector{Int8},Number}()
+    result = Dict{Vector{Int8},ComplexF64}()
     for (key1, value1) in sentence1
         for (key2, value2) in sentence2
             string, sign, _ = pauliprod(key1, key2)
@@ -114,23 +73,23 @@ function _pauliprod(sentence1::Dict{Vector{Int8},Number},
     return result
 end
 
-function pauliprod(sentences::Dict{Vector{Int8},Number}...;
-    tol=0.0)::Dict{Vector{Int8},Number}
+function pauliprod(sentences::AbstractDict{<:AbstractVector{Int8},<:Number}...;
+    atol::Real=0)::Dict{Vector{Int8},<:Number}
 
     result = sentences[1]
     for sentence in sentences[2:end]
         result = _pauliprod(result, sentence)
     end
     for (key, value) in result
-        abs(value) <= tol && pop!(result, key)
+        abs(value) <= atol && pop!(result, key)
     end
     length(sentences) > 1 ? (return result) : return copy(sentences[1])
 end
 
-function _conjugate(sentence::Dict{Vector{Int8},Float64},
-    generator::Vector{Int8},
-    angle::Float64;
-    tol::Float64=0.0)::Dict{Vector{Int8},Float64}
+function _conjugate(sentence::AbstractDict{<:AbstractVector{Int8},Float64},
+    generator::AbstractVector{Int8},
+    angle::Real;
+    atol::Real=0)::Dict{Vector{Int8},Float64}
 
     # trash = Matrix{Int8}(undef, length(generator), 0)
     # for (key, value) in sentence
@@ -153,37 +112,17 @@ function _conjugate(sentence::Dict{Vector{Int8},Float64},
         else
             result[key] = get(result, key, 0.0) + value * cos(2 * angle)
             result[string] = get(result, string, 0.0) - imag(sign) * value * sin(2 * angle)
-            abs(result[key]) <= tol && pop!(result, key)
-            abs(result[string]) <= tol && pop!(result, string)
+            abs(result[key]) <= atol && pop!(result, key)
+            abs(result[string]) <= atol && pop!(result, string)
         end
     end
     return result
 end
 
-function _conjugate(sentence::Dict{Vector{Int8},Float64},
-    generator::SubArray,
-    angle::Float64;
-    tol::Float64=0.0)::Dict{Vector{Int8},Float64}
-
-    result = Dict{Vector{Int8},Float64}()
-    for (key, value) in sentence
-        string, sign, c = pauliprod(generator, key)
-        if c
-            result[key] = get(result, key, 0.0) + value
-        else
-            result[key] = get(result, key, 0.0) + value * cos(2 * angle)
-            result[string] = get(result, string, 0.0) - imag(sign) * value * sin(2 * angle)
-            abs(result[key]) <= tol && pop!(result, key)
-            abs(result[string]) <= tol && pop!(result, string)
-        end
-    end
-    return result
-end
-
-function conjugate(sentence::Dict{Vector{Int8},Float64},
-    generators::Matrix{Int8},
-    angles::Vector{Float64};
-    tol=0.0)
+function conjugate(sentence::AbstractDict{<:AbstractVector{Int8},Float64},
+    generators::AbstractMatrix{Int8},
+    angles::AbstractVector{<:Real};
+    atol::Real=0)::Dict{Vector{Int8},Float64}
 
     size(generators, 2) == length(angles) || throw(DimensionMismatch("Generators and angles need to have equal size ($(size(generators, 2)) and $(length(angles)))"))
 
@@ -194,28 +133,7 @@ function conjugate(sentence::Dict{Vector{Int8},Float64},
     #         Threads.@spawn _conjugate(Dict(chunk), view(generators, :, i), angles[i], tol=tol)
     #     end
     #     result = paulisum(fetch.(tasks)..., tol=tol)
-        result = _conjugate(result, generators[:, i], angles[i], tol=tol)
+        result = _conjugate(result, generators[:, i], angles[i], atol=atol)
     end
     length(angles) > 0 ? (return result) : return copy(sentence)
 end
-
-function conjugate(sentence::Dict{Vector{Int8},Float64},
-    generators::SubArray,
-    angles::Vector{Float64};
-    tol=0.0)
-
-    size(generators, 2) == length(angles) || throw(DimensionMismatch("Generators and angles need to have equal size ($(size(generators, 2)) and $(length(angles)))"))
-
-    result = sentence
-    for i in eachindex(angles)[end:-1:1]
-    #     chunks = Iterators.partition(result, max(1, length(result) ÷ Threads.nthreads()))
-    #     tasks = map(chunks) do chunk
-    #         Threads.@spawn _conjugate(Dict(chunk), view(generators, :, i), angles[i], tol=tol)
-    #     end
-    #     result = paulisum(fetch.(tasks)..., tol=tol)
-        result = _conjugate(result, generators[:, i], angles[i], tol=tol)
-    end
-    length(angles) > 0 ? (return result) : return copy(sentence)
-end
-
-
