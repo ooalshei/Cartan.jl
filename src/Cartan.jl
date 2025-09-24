fdbe3b7255da5f09d8618fd48beeb5d4579705f7
ooalshei <ooalshei@ncsu.edu>
2025-09-24 10:43:41 -0400

    Added SymplecticPauli.jl as a dependency

--- src/Cartan.jl
+++ src/Cartan.jl
@@ -1,16 +1,8 @@
 module Cartan
 
-@static if Sys.isunix()
-    run(`bash $(@__DIR__)/../install_git.sh`)
-elseif Sys.iswindows()
-    run(`cmd /c $(@__DIR__)/../install_git.cmd`)
-end
-run(pipeline(`git -C $(@__DIR__)/.. submodule update --init`, stdout=devnull))
-
 using Reexport
-include("../SymplecticPauli.jl/src/SymplecticPauli.jl")
-@reexport using .SymplecticPauli: AbstractPauli, UPauli, Pauli, PauliList, PauliSentence
-using .SymplecticPauli: com, counti, countx, county, countz, ad, ad!
+@reexport using SymplecticPauli: AbstractPauli, UPauli, Pauli, PauliList, PauliSentence
+using SymplecticPauli: com, counti, countx, county, countz, ad, ad!
 
 export hamiltonian,
     algebra,
